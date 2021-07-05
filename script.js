ea4b363fa9a3e8f45b6f607453188045975bfa73
ResminiLoris <resminiloris@gmail.com>
2021-07-05 17:38:51 +0200

    console

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-var cpNumbers = new Array(100);
-for (var i = 0; i < 100; i++) {
-    cpNumbers[i] = i + 1; 
-}
 
-for (var i = 0; i < cpNumbers.length; i++) {
-    console.log(cpNumbers[i]);
+for (var i = 1; i <= 100; i++) {
+    var stampa = i;
     if (i % 3 == 0){
-        i = "fizz";
+        stampa = "fizz";
     }
+    if (i % 5 == 0){
+        stampa = "buzz";
+    }
+    if (i % 3 == 0 && i % 5 == 0 ){
+        stampa = "fizzbuzz";
+    }
+    console.log(stampa);
 }
